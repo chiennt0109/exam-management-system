ff296075b5711d9f6b28e01d0b190a628962e9fb
chiennt0109 <chiennt0109@gmail.com>
2026-05-21 17:05:15 +0700

    Fix PHP 8 string helper compatibility for bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,30 @@
 <?php
+if (!function_exists('str_contains')) {
+    function str_contains(string $haystack, string $needle): bool
+    {
+        return $needle === '' || strpos($haystack, $needle) !== false;
+    }
+}
+
+if (!function_exists('str_starts_with')) {
+    function str_starts_with(string $haystack, string $needle): bool
+    {
+        return $needle === '' || strpos($haystack, $needle) === 0;
+    }
+}
+
+if (!function_exists('str_ends_with')) {
+    function str_ends_with(string $haystack, string $needle): bool
+    {
+        if ($needle === '') {
+            return true;
+        }
+
+        $length = strlen($needle);
+        return substr($haystack, -$length) === $needle;
+    }
+}
+
 if (!defined('BASE_PATH')) {
     define('BASE_PATH', __DIR__);
 }
